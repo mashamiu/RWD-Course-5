94358e7ed3f5e4ff066840e04528f1cb92fcfb0d
mashamiu <50354387+mashamiu@users.noreply.github.com>
2019-05-07 11:36:18 +0300

    Delete blog.js

--- server/blog.js
+++
@@ -1,8 +0,0 @@
-Blog.config({
-  title: "Language Link",
-
-  rss: {
-    title: 'Language Link - Professional English, Ukrainian and Russian language services',
-    description: 'Quality translation, editing and proofreading English, Ukrainian and Russian language services'
-  }
-});
